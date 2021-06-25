78f0dd56c88b106295686a7e50dfdac1b2fe96fb
Abhishek Shandilya <schandilya.abhi007@gmail.com>
2021-06-25 13:02:17 +0530

    fibonacci via memoization

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,12 @@
-def fibonacci(count):
+def fibonacci(count, memo={}):
+	if count in memo: return memo[count]
+
 	if count == 0: return 0 
 	if count == 1: return 1
 
-	return fibonacci(count - 1) + fibonacci(count - 2)
+	sum =  fibonacci(count - 1) + fibonacci(count - 2)
+	memo[count] = sum
+	return sum
 
 if __name__ == "__main__":
     print(fibonacci(40))
