934783804a873278ab950b0f7d92c632f52273e9
Abhishek Shandilya <schandilya.abhi007@gmail.com>
2021-06-28 14:34:56 +0530

    added count_construct via memoization

---
+++ count_construct.py
@@ -0,0 +1,26 @@
+def count_construct(targetStr: str, arr: list, memo = None) -> bool:
+	if memo == None: memo = {}
+
+	if targetStr in memo: return memo[targetStr]
+
+	if targetStr == '': return 1
+
+	total_count = 0
+
+	for word in arr:
+		if targetStr.startswith(word):  # if the target/remaiing string starts with the word from wordbank
+			remaining_string = targetStr.removeprefix(word)  # for python < 3.9 use --> x[len(y):]
+			number_of_ways =  count_construct(remaining_string, arr, memo)
+			total_count += number_of_ways
+	
+	memo[targetStr] = total_count
+	return total_count
+
+if __name__ == '__main__':
+	print(count_construct('purple', ['purp', 'p', 'ur', 'le', 'purpl']))  # Result: 2
+	print(count_construct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd']))  # Result: 1
+	print(count_construct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']))  # Result: 0
+	print(count_construct('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't']))  # Result: 4
+	print(count_construct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', [
+		'e', 'ee', 'eee', 'eeee', 'eeeee', 'eeeeee'
+	])) # Result : 0
