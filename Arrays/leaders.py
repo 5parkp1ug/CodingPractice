e20ef56a206b2d6046711c04694d46565e3d6017
Abhishek Shandilya <schandilya.abhi007@gmail.com>
2021-07-19 16:00:40 +0530

    Fixed issue with leaders problem

--- Arrays/leaders.py
+++ Arrays/leaders.py
@@ -5,20 +5,20 @@
 #                the right side.
 # ==============================================================================
 
-def leaders(arr: list) -> list:
+def leaders(arr: list) -> None:
     length = len(arr)
-    temp = []
-    for index, item in enumerate(arr):
-        if index == length-1:
-            temp.append(item)
-        elif item > arr[index+1]:
-            temp.append(item)
-    return temp
+    current_leader = arr[length-1]
+    print(current_leader)
+
+    for index in range(length-2, -1, -1):
+        if current_leader < arr[index]:
+            current_leader = arr[index]
+            print(current_leader)
 
 if __name__ == '__main__':
-    array = [7, 10, 4, 3, 6, 5, 2]  # Result: [10, 4, 6, 5, 2]
+    array = [7, 10, 4, 3, 6, 5, 2]  # Result: [10, 6, 5, 2]
     array1 = [1,2,3,4,5,6,7,8,9]  # Result: [9]
+    print(f'Leaders of array {array}')
     result = leaders(array)
+    print(f'Leaders of array {array1}')
     result1 = leaders(array1)
-    print(f'The leaders of given array are - {result}')
-    print(f'The leaders of given array are - {result1}')
